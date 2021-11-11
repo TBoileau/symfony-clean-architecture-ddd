51bb0bf72d1d0870635423cf0a67999641f4f429
TBoileau <t-boileau@email.com>
2021-11-11 20:04:09 +0100

    feat (security): implement in memory UserRepository  (#45)

---
+++ src/Security/Infrastructure/InMemory/Repository/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Infrastructure\InMemory\Repository;
+
+use App\Security\Domain\Entity\User;
+use App\Security\Domain\Gateway\UserGateway;
+use App\Security\Infrastructure\InMemory\Factory\UserFactory;
+
+final class UserRepository implements UserGateway
+{
+    public function getUserByEmail(string $email): ?User
+    {
+        return UserFactory::createOne();
+    }
+}
