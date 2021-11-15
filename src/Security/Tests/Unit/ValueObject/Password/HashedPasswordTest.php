f5fa9ce8216228913ae5e9234369ddb2c57a8124
TBoileau <t-boileau@email.com>
2021-11-15 09:22:06 +0100

    feat (security): add verify method (#83)

--- src/Security/Tests/Unit/ValueObject/Password/HashedPasswordTest.php
+++ src/Security/Tests/Unit/ValueObject/Password/HashedPasswordTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Security\Tests\Unit\ValueObject\Password;
 
+use App\Security\Domain\PasswordHasher\PasswordHasherInterface;
 use App\Security\Domain\ValueObject\Password\HashedPassword;
+use App\Security\Domain\ValueObject\Password\PlainPassword;
 use PHPUnit\Framework\TestCase;
 
 final class HashedPasswordTest extends TestCase
@@ -13,5 +15,14 @@ final class HashedPasswordTest extends TestCase
     {
         $hashedPassword = HashedPassword::createFromString('test');
         $this->assertEquals('test', (string) $hashedPassword);
+
+        $plainPassword = PlainPassword::createFromString('test');
+
+        $this->assertTrue($hashedPassword->verify(new class() implements PasswordHasherInterface {
+            public function verifyPassword(HashedPassword $hashedPassword, PlainPassword $plainPassword): bool
+            {
+                return (string) $hashedPassword === (string) $plainPassword;
+            }
+        }, $plainPassword));
     }
 }
