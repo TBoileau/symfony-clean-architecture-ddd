e65090d33eae5d899bb7d5b8f0b2d17d96bc7390
TBoileau <t-boileau@email.com>
2021-11-16 10:56:20 +0100

    feat (shared): implement Interval (#112)

---
+++ src/Shared/Domain/ValueObject/Date/Interval.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Domain\ValueObject\Date;
+
+use App\Shared\Domain\Exception\InvalidArgumentException;
+use DateInterval;
+use Exception;
+
+final class Interval
+{
+    private function __construct(public string $interval)
+    {
+    }
+
+    public static function createFromString(string $interval): Interval
+    {
+        try {
+            new DateInterval($interval);
+        } catch (Exception) {
+            throw new InvalidArgumentException('Interval invalid.');
+        }
+
+        return new Interval($interval);
+    }
+
+    public function toDateInterval(): DateInterval
+    {
+        return new DateInterval($this->interval);
+    }
+}
