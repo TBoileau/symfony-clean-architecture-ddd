462dd04763801a3f140fee4808e426baa7a0276f
TBoileau <t-boileau@email.com>
2021-11-17 18:06:08 +0100

    test (security): update security e2e test (#138)

--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Security\Domain\Contract\Gateway\UserGateway;
+use App\Security\Domain\Entity\User;
+use App\Shared\Domain\ValueObject\Email\EmailAddress;
 use Blackfire\Bridge\PhpUnit\BlackfireTestCase;
 use Blackfire\Build\BuildHelper;
 
@@ -30,11 +33,26 @@ final class SecurityTest extends BlackfireTestCase
             'request_forgotten_password[email]' => 'user+1@email.com',
         ]);
 
+        static::bootKernel();
+
+        /** @var User $user */
+        $user = static::getContainer()
+            ->get(UserGateway::class)
+            ->getUserByEmail(EmailAddress::createFromString('user+1@email.com'));
+
+        $client->request('GET', sprintf('/security/reset-password/%s', (string) $user->forgottenPasswordToken));
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Reset', [
+            'reset_password[password]' => 'new_password',
+        ]);
+
         $this->assertResponseIsSuccessful();
 
         $client->submitForm('Sign in', [
             'email' => 'user+1@email.com',
-            'password' => 'password',
+            'password' => 'new_password',
         ]);
 
         $this->assertResponseIsSuccessful();
