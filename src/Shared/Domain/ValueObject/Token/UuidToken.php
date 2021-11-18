665395b37597be0449e0e3febab9bedc3380ac74
Thomas Boileau <t-boileau@email.com>
2021-11-18 12:33:58 +0100

    License (#154)
    * build: add copyright (#153)
    
    * docs: add license (#145)

--- src/Shared/Domain/ValueObject/Token/UuidToken.php
+++ src/Shared/Domain/ValueObject/Token/UuidToken.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Copyright (C) Thomas Boileau - All Rights Reserved.
+ *
+ * This source code is protected under international copyright law.
+ * All rights reserved and protected by the copyright holders.
+ * This file is confidential and only available to authorized individuals with the
+ * permission of the copyright holders. If you encounter this file and do not have
+ * permission, please contact the copyright holders and delete this file.
+ */
+
 declare(strict_types=1);
 
 namespace App\Shared\Domain\ValueObject\Token;
